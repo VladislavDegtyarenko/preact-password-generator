6eca65b8fda79bcbbaf0b0b156e2b6136175a99d
vdihtiarenko <v.dihtiarenko@astoundcommerce.com>
2022-08-25 17:00:39 +0300

    set base dir

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+   base: "/react-password-generator/",
    plugins: [react()],
    server: { https: true },
 });
