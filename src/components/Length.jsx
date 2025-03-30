001721d375459768cbbeb19f041277f84970a519
Vladyslav Dihtiarenko <vladislavdegtyarenko@gmail.com>
2025-03-31 01:51:03 +0300

    fix min length value

--- src/components/Length.jsx
+++ src/components/Length.jsx
@@ -7,8 +7,22 @@ function Length(props) {
       <label className="length">
          Password Length
          <div>
-            <input type="number" value={value} name="length" onChange={handleChange} />
-            <input type="range" min="1" max="40" step="1" value={value} name="length" onChange={handleChange} />
+            <input
+               type="number"
+               value={value}
+               min={1}
+               name="length"
+               onChange={handleChange}
+            />
+            <input
+               type="range"
+               min="1"
+               max="40"
+               step="1"
+               value={value}
+               name="length"
+               onChange={handleChange}
+            />
          </div>
       </label>
    );
